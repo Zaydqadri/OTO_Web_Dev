2be5befb73846c83d3cee7ffed0afe368c6b0274
Muteeb Syed <msyd27@hotmail.com>
2025-10-11 19:51:04 -0400

    changed favicon png

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Ontario Taraweeh Outreach</title>
         <meta name="description" content="Connecting Huffaz and Masajid across Ontario." />
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.png" />
         <meta name="theme-color" content="#021733" />
       </Head>
 
